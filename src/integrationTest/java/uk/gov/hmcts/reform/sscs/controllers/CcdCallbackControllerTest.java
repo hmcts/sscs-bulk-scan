9f48d829ad6f8f26ccc6b7f0dfefca29cfed8cc8
nitinprabhu <prabhunitin61@gmail.com>
2018-10-29 09:10:47 +0000

    Added empty test

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/sscs/controllers/CcdCallbackControllerTest.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.sscs.controllers;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CcdCallbackControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+    
+
+}
