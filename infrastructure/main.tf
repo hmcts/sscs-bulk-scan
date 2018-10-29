489669308d3760f8f9fa6069cad52573643216e8
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2018-10-29 09:31:29 +0000

    Updated ccd url

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,7 +10,7 @@ locals {
   ase_name  = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
   local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
   s2s_url   = "http://rpe-service-auth-provider-${local.local_env}.service.core-compute-${local.local_env}.internal"
-  ccdApiUrl = "http://ccd-data-store-api-${local.local_env}.service.${local.local_env}.internal"
+  ccdApiUrl = "http://ccd-data-store-api-${local.local_env}.service.core-compute-${local.local_env}.internal"
 }
 
 module "sscs-bulk-scan" {
