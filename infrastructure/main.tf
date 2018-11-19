d2eb51fba139e6d217ae5b7fcf4b6b898f197d79
jackmaloney <jack.maloney@transformuk.com>
2018-11-19 14:16:38 +0000

    Add idam url for each env

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,7 +10,7 @@ locals {
   ase_name  = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
   local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
 
-  s2sCnpUrl   = "http://rpe-service-auth-provider-${local.local_env}.service.core-compute-${local.local_env}.internal"
+  s2sUrl   = "http://rpe-service-auth-provider-${local.local_env}.service.core-compute-${local.local_env}.internal"
   ccdApiUrl = "http://ccd-data-store-api-${local.local_env}.service.core-compute-${local.local_env}.internal"
 
   vaultName = "sscs-bulk-scan-${local.local_env}"
@@ -35,17 +35,17 @@ module "sscs-bulk-scan" {
 
     CORE_CASE_DATA_API_URL = "${local.ccdApiUrl}"
 
-    IDAM_URL = "${data.azurerm_key_vault_secret.idam-api.value}"
+    IDAM_URL = "${var.idam_url}"
 
-    IDAM.S2S-AUTH.TOTP_SECRET = "${data.azurerm_key_vault_secret.sscs-s2s-secret.value}"
-    IDAM.S2S-AUTH = "${local.s2sCnpUrl}"
-    IDAM.S2S-AUTH.MICROSERVICE = "${var.idam_s2s_auth_microservice}"
+    IDAM_S2S_AUTH_TOTP_SECRET = "${data.azurerm_key_vault_secret.sscs_s2s_secret.value}"
+    IDAM_S2S_AUTH = "${local.s2sUrl}"
+    IDAM_S2S_AUTH_MICROSERVICE = "${var.idam_s2s_auth_microservice}"
 
-    IDAM_SSCS_SYSTEMUPDATE_USER = "${data.azurerm_key_vault_secret.idam-sscs-systemupdate-user.value}"
-    IDAM_SSCS_SYSTEMUPDATE_PASSWORD = "${data.azurerm_key_vault_secret.idam-sscs-systemupdate-password.value}"
+    IDAM_OAUTH2_USER_EMAIL = "${data.azurerm_key_vault_secret.idam_sscs_systemupdate_user.value}"
+    IDAM_OAUTH2_USER_PASSWORD = "${data.azurerm_key_vault_secret.idam_sscs_systemupdate_password.value}"
 
     IDAM_OAUTH2_CLIENT_ID = "${var.idam_oauth2_client_id}"
-    IDAM_OAUTH2_CLIENT_SECRET = "${data.azurerm_key_vault_secret.idam-sscs-oauth2-client-secret.value}"
+    IDAM_OAUTH2_CLIENT_SECRET = "${data.azurerm_key_vault_secret.idam_sscs_oauth2_client_secret.value}"
     IDAM_OAUTH2_REDIRECT_URL = "${var.idam_redirect_url}"
 
   }
@@ -62,27 +62,23 @@ module "sscs-bulk-scan-vault" {
   product_group_object_id = "70de400b-4f47-4f25-a4f0-45e1ee4e4ae3"
 }
 
-data "azurerm_key_vault_secret" "sscs-s2s-secret" {
+data "azurerm_key_vault_secret" "sscs_s2s_secret" {
   name      = "sscs-s2s-secret"
   vault_uri = "${module.sscs-bulk-scan-vault.key_vault_uri}"
 }
 
-data "azurerm_key_vault_secret" "idam-sscs-systemupdate-user" {
+data "azurerm_key_vault_secret" "idam_sscs_systemupdate_user" {
   name      = "idam-sscs-systemupdate-user"
   vault_uri = "${module.sscs-bulk-scan-vault.key_vault_uri}"
 }
 
-data "azurerm_key_vault_secret" "idam-sscs-systemupdate-password" {
+data "azurerm_key_vault_secret" "idam_sscs_systemupdate_password" {
   name      = "idam-sscs-systemupdate-password"
   vault_uri = "${module.sscs-bulk-scan-vault.key_vault_uri}"
 }
 
-data "azurerm_key_vault_secret" "idam-sscs-oauth2-client-secret" {
+data "azurerm_key_vault_secret" "idam_sscs_oauth2_client_secret" {
   name      = "idam-sscs-oauth2-client-secret"
   vault_uri = "${module.sscs-bulk-scan-vault.key_vault_uri}"
 }
 
-data "azurerm_key_vault_secret" "idam-api" {
-  name      = "idam-api"
-  vault_uri = "${module.sscs-bulk-scan-vault.key_vault_uri}"
-}
