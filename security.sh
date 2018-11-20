10b1b449ce65a2e9e9f5b63a90be44e2aee65b81
satya <1426759+satyachundur@users.noreply.github.com>
2018-11-20 14:59:19 +0000

    Update security.sh

--- security.sh
+++ security.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-while !(curl -s http://0.0.0.0:1001) > /dev/null
-do
-     i=$(( (i+1) %4 ))
-     sleep .1
-done
-echo "ZAP has successfully started"
 echo ${TEST_URL}
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -r api-report.html
 cp *.html functional-output/
