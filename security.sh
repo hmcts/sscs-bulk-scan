91a2177b7b961cb1fe7bf154bcf724a19b08be15
satya <1426759+satyachundur@users.noreply.github.com>
2018-11-22 16:27:58 +0000

    Update security.sh

--- security.sh
+++ security.sh
@@ -3,5 +3,5 @@ echo ${TEST_URL}
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -r api-report.html -a 
 ls
 cat zap.out
-cp /wrk/api-report.html functional-output/
+cp wrk/api-report.html functional-output/
 zap-cli -p 1001 alerts -l Informational
