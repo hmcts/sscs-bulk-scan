81ca1849b72f67665024a284bc3142d1137b7773
satya <1426759+satyachundur@users.noreply.github.com>
2018-11-22 15:49:00 +0000

    Update security.sh

--- security.sh
+++ security.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
-zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -z -config api.addrs.addr.name=0.0.0.0 -r api-report.html -g gen_file
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -z "-config api.addrs.addr.name=0.0.0.0" -r api-report.html -g gen_file
 ls
 cat zap.out
 cp /zap/wrk/api-report.html functional-output/
