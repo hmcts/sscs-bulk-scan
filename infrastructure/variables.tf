8ae79ecc7c8544e5b60888668c4b494913029a0b
jackmaloney <jack.maloney@transformuk.com>
2018-11-22 08:48:09 +0000

    Update idam redirect url for environments

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -52,7 +52,7 @@ variable "idam_oauth2_client_id" {
 }
 
 variable "idam_redirect_url" {
-  default = "https://sscs-case-loader-sandbox.service.core-compute-sandbox.internal"
+  default = "https://rpe-bulk-scan-processor*.service.core-compute-saat.internal/"
 }
 
 variable "idam_url" {
