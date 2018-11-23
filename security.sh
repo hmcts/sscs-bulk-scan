2a830aa4c0b950085f531548316bc71fb738e4ff
satya <1426759+satyachundur@users.noreply.github.com>
2018-11-23 16:45:12 +0000

    Update security.sh

--- security.sh
+++ security.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
-zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -r /zap/api-report_html -a 
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -a 
 ls
 cat zap.out
 echo "the current listing of files"
