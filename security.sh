685e5ebcff6303eb9974d33f967cf113ddac74d8
Venu Ancha <vancha@hmcts.net>
2018-11-23 16:56:44 +0000

    remove ls

--- security.sh
+++ security.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
-zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -a 
-ls
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -a
 cat zap.out
-echo "the current listing of files"
-ls -la
+zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
 echo "listings of zap folder"
 ls -la /zap
-echo "listings of root folder"
-ls /
-echo "listings of wrk folder"
-ls -la /zap/wrk
-zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
 cp /zap/api-report.html functional-output/
 zap-cli -p 1001 alerts -l Informational
