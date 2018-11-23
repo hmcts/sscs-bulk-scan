93801a21520db3d22e80b93b872a6eb6714da693
satya <1426759+satyachundur@users.noreply.github.com>
2018-11-23 12:30:15 +0000

    Update security.sh

--- security.sh
+++ security.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
+echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
+chown -R $(id -u):$(id -u) wrk/
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -r api-report.html -a 
 ls
 cat zap.out
@@ -11,6 +13,6 @@ ls -la /zap
 echo "listings of root folder"
 ls /
 echo "listings of wrk folder"
-ls -la ./wrk
+ls -la /zap/wrk
 cp ./wrk/api-report.html functional-output/
 zap-cli -p 1001 alerts -l Informational
