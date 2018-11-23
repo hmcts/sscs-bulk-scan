49d01cb918ec8c883b1a7d3a52bb16a4c23bfa85
satya <1426759+satyachundur@users.noreply.github.com>
2018-11-23 15:06:51 +0000

    Update security.sh

--- security.sh
+++ security.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
-zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -r /zap/wrk/api-report.html -a 
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -r api-report.html -a 
 ls
 cat zap.out
-pwd
 echo "the current listing of files"
 ls -la
 echo "listings of zap folder"
@@ -12,5 +11,5 @@ echo "listings of root folder"
 ls /
 echo "listings of wrk folder"
 ls -la /zap/wrk
-cp ./wrk/api-report.html functional-output/
+cp api-report.html functional-output/
 zap-cli -p 1001 alerts -l Informational
