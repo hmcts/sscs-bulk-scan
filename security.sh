e411016ad43ff811388039f11940be32ddf0daf5
satya <1426759+satyachundur@users.noreply.github.com>
2018-11-23 13:06:11 +0000

    Update security.sh

--- security.sh
+++ security.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
-echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
-chown -R $(id -u):$(id -u) wrk/
-zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -r api-report.html -a 
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -r /zap/wrk/api-report.html -a 
 ls
 cat zap.out
 pwd
