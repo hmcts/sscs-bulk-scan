fa2c36216c52dcf6464ae7c6e5ba733e39fc6b0e
Ryan Da Costa <ryan.dacosta@hmcts.net>
2019-04-17 10:28:35 +0100

    add some debug

--- src/smokeTest/java/uk/gov/hmcts/reform/sscs/GetSmokeCase.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/sscs/GetSmokeCase.java
@@ -8,7 +8,10 @@ import java.io.IOException;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,16 +22,36 @@ import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
 @RunWith(SpringRunner.class)
 public class GetSmokeCase {
 
+    private static final Logger LOG = LoggerFactory.getLogger(GetSmokeCase.class);
+
     private final String appUrl = System.getenv("TEST_URL");
 
     @Autowired
     private IdamService idamService;
+
+    @Value("${idam.oauth2.user.email}")
+    private String idamOauth2UserEmail;
+
+    @Value("${idam.oauth2.user.password}")
+    private String idamOauth2UserPassword;
+
+    @Value("${idam.oauth2.client.id}")
+    private String idamOauth2ClientId;
+
+    @Value("${idam.oauth2.client.secret}")
+    private String idamOauth2ClientSecret;
+
+    @Value("${idam.oauth2.redirectUrl}")
+    private String idamOauth2RedirectUrl;
 
     @Test
     public void givenASmokeCase_retrieveFromCcd() throws IOException {
         RestAssured.baseURI = appUrl;
         RestAssured.useRelaxedHTTPSValidation();
 
+        LOG.info("values are idamOauth2UserEmail {}, idamOauth2UserPassword {}, "
+                + "idamOauth2ClientId {}, idamOauth2ClientSecret {}, idamOauth2RedirectUrl {}",
+            idamOauth2UserEmail, idamOauth2UserPassword, idamOauth2ClientId, idamOauth2ClientSecret, idamOauth2RedirectUrl);
         IdamTokens idamTokens = idamService.getIdamTokens();
 
         List<String> errors = RestAssured
