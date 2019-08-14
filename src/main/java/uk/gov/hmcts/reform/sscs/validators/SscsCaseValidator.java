e03d4aa7288e6bf887df86dabca8b736c9031040
Chris Moreton <chris@netsensia.com>
2019-08-14 13:44:19 +0100

    Warn when phone number greater than 11 digits

--- src/main/java/uk/gov/hmcts/reform/sscs/validators/SscsCaseValidator.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/validators/SscsCaseValidator.java
@@ -25,7 +25,11 @@ import uk.gov.hmcts.reform.sscs.service.RegionalProcessingCenterService;
 @Slf4j
 public class SscsCaseValidator implements CaseValidator {
 
-    private static final String PHONE_REGEX = "^[0-9\\-+ ]{10,17}$";
+    private static final String PHONE_REGEX =
+        "^((?:(?:\\(?(?:0(?:0|11)\\)?[\\s-]?\\(?|\\+)\\d{1,4}\\)?[\\s-]?(?:\\(?0\\)?[\\s-]?)?)|(?:\\(?0))(?:" +
+            "(?:\\d{5}\\)?[\\s-]?\\d{4,5})|(?:\\d{4}\\)?[\\s-]?(?:\\d{5}|\\d{3}[\\s-]?\\d{3}))|(?:\\d{3}\\)" +
+            "?[\\s-]?\\d{3}[\\s-]?\\d{3,4})|(?:\\d{2}\\)?[\\s-]?\\d{4}[\\s-]?\\d{4}))(?:[\\s-]?(?:x|ext\\.?|\\#)" +
+            "\\d{3,4})?)?$";
 
     List<String> warnings;
     List<String> errors;
