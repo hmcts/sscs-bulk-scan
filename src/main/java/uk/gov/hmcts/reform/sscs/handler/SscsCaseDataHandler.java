1f1f662ebda7942aa01933ea3ab7f56cfd628fb1
srochani <sunil.rochani@hotmail.com>
2019-09-02 15:23:51 +0100

    SSCS-6067 Bulk scan - Duplicate cases getting created from exception record

--- src/main/java/uk/gov/hmcts/reform/sscs/handler/SscsCaseDataHandler.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/handler/SscsCaseDataHandler.java
@@ -4,17 +4,30 @@ import static org.springframework.util.ObjectUtils.isEmpty;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.SEND_TO_DWP;
 
 import feign.FeignException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 import uk.gov.hmcts.reform.sscs.bulkscancore.ccd.CaseDataHelper;
 import uk.gov.hmcts.reform.sscs.bulkscancore.domain.CaseResponse;
 import uk.gov.hmcts.reform.sscs.bulkscancore.domain.HandlerResponse;
 import uk.gov.hmcts.reform.sscs.bulkscancore.domain.Token;
 import uk.gov.hmcts.reform.sscs.bulkscancore.handlers.CaseDataHandler;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
+import uk.gov.hmcts.reform.sscs.ccd.domain.SscsCaseDetails;
+import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
 import uk.gov.hmcts.reform.sscs.domain.CaseEvent;
 import uk.gov.hmcts.reform.sscs.exceptions.CaseDataHelperException;
 import uk.gov.hmcts.reform.sscs.helper.SscsDataHelper;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
+import uk.gov.hmcts.reform.sscs.idam.IdamTokens;
+
 
 @Component
 @Slf4j
@@ -23,13 +36,19 @@ public class SscsCaseDataHandler implements CaseDataHandler {
     private final SscsDataHelper sscsDataHelper;
     private final CaseDataHelper caseDataHelper;
     private final CaseEvent caseEvent;
+    private final CcdService ccdService;
+    private final IdamService idamService;
 
     public SscsCaseDataHandler(SscsDataHelper sscsDataHelper,
                                CaseDataHelper caseDataHelper,
-                               CaseEvent caseEvent) {
+                               CaseEvent caseEvent,
+                               CcdService ccdService,
+                               IdamService idamService) {
         this.sscsDataHelper = sscsDataHelper;
         this.caseDataHelper = caseDataHelper;
         this.caseEvent = caseEvent;
+        this.ccdService = ccdService;
+        this.idamService = idamService;
     }
 
     public CallbackResponse handle(CaseResponse caseValidationResponse,
@@ -37,18 +56,48 @@ public class SscsCaseDataHandler implements CaseDataHandler {
                                    Token token,
                                    String exceptionRecordId) {
 
-        if (canCreateCase(caseValidationResponse, ignoreWarnings)) {
+        if (canCreateCase(caseValidationResponse, ignoreWarnings, exceptionRecordId)) {
+            boolean isCaseAlreadyExists = false;
             String eventId = sscsDataHelper.findEventToCreateCase(caseValidationResponse);
 
+            String caseReference = String.valueOf(Optional.ofNullable(caseValidationResponse.getTransformedCase().get("caseReference")).orElse(""));
+            String generatedNino = String.valueOf(Optional.ofNullable(caseValidationResponse.getTransformedCase().get("generatedNino")).orElse(""));
+            String benefitType = String.valueOf(Optional.ofNullable(caseValidationResponse.getTransformedCase().get("benefitCode")).orElse(""));
+            Appeal appeal = (Appeal) caseValidationResponse.getTransformedCase().get("appeal");
+            String mrnDate = appeal.getMrnDetails().getMrnDate();
+            Long caseId = null;
+
+            if (!StringUtils.isEmpty(caseReference)) {
+                log.info("Case already exists for exception record id {}", caseReference, exceptionRecordId);
+                isCaseAlreadyExists = true;
+            } else if (!StringUtils.isEmpty(generatedNino) && !StringUtils.isEmpty(benefitType) && !StringUtils.isEmpty(mrnDate)) {
+                IdamTokens idamTokens = idamService.getIdamTokens();
+                Map<String,String> searchCriteria = new HashMap<>();
+                searchCriteria.put("case.generatedNino", generatedNino);
+                searchCriteria.put("case.appeal.benefitType.code", benefitType);
+                searchCriteria.put("case.appeal.mrnDetails.mrnDate", mrnDate);
+
+                List<SscsCaseDetails> caseDetails = ccdService.findCaseBy(searchCriteria, idamTokens);
+
+                if (!CollectionUtils.isEmpty(caseDetails)) {
+                    log.info("Duplicate case found for Nino {} , benefit type {} and mrnDate {}. "
+                                    + "No need to continue with post create case processing.",
+                            generatedNino, benefitType, mrnDate);
+                    isCaseAlreadyExists = true;
+                }
+            }
+
             try {
-                Long caseId = caseDataHelper.createCase(caseValidationResponse.getTransformedCase(), token.getUserAuthToken(), token.getServiceAuthToken(), token.getUserId(), eventId);
 
-                log.info("Case created with caseId {} from exception record id {}", caseId, exceptionRecordId);
+                if (!isCaseAlreadyExists) {
+                    caseId = caseDataHelper.createCase(caseValidationResponse.getTransformedCase(), token.getUserAuthToken(), token.getServiceAuthToken(), token.getUserId(), eventId);
+                    log.info("Case created with caseId {} from exception record id {}", caseId, exceptionRecordId);
 
-                if (isCaseCreatedEvent(eventId)) {
-                    log.info("About to update case with sendToDwp event for id {}", caseId);
-                    caseDataHelper.updateCase(caseValidationResponse.getTransformedCase(), token.getUserAuthToken(), token.getServiceAuthToken(), token.getUserId(), SEND_TO_DWP.getCcdType(), caseId, "Send to DWP", "Send to DWP event has been triggered from Bulk Scan service");
-                    log.info("Case updated with sendToDwp event for id {}", caseId);
+                    if (isCaseCreatedEvent(eventId)) {
+                        log.info("About to update case with sendToDwp event for id {}", caseId);
+                        caseDataHelper.updateCase(caseValidationResponse.getTransformedCase(), token.getUserAuthToken(), token.getServiceAuthToken(), token.getUserId(), SEND_TO_DWP.getCcdType(), caseId, "Send to DWP", "Send to DWP event has been triggered from Bulk Scan service");
+                        log.info("Case updated with sendToDwp event for id {}", caseId);
+                    }
                 }
 
                 return HandlerResponse.builder().state("ScannedRecordCaseCreated").caseId(String.valueOf(caseId)).build();
@@ -65,7 +114,7 @@ public class SscsCaseDataHandler implements CaseDataHandler {
         return eventId.equals(caseEvent.getCaseCreatedEventId()) || eventId.equals(caseEvent.getValidAppealCreatedEventId());
     }
 
-    private Boolean canCreateCase(CaseResponse caseValidationResponse, boolean ignoreWarnings) {
+    private Boolean canCreateCase(CaseResponse caseValidationResponse, boolean ignoreWarnings, String exceptionRecordId) {
         return ((!isEmpty(caseValidationResponse.getWarnings()) && ignoreWarnings) || isEmpty(caseValidationResponse.getWarnings()));
     }
 
--- src/test/java/uk/gov/hmcts/reform/sscs/handler/SscsCaseDataHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/handler/SscsCaseDataHandlerTest.java
@@ -2,9 +2,9 @@ package uk.gov.hmcts.reform.sscs.handler;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static uk.gov.hmcts.reform.sscs.ccd.domain.EventType.SEND_TO_DWP;
 import static uk.gov.hmcts.reform.sscs.common.TestHelper.*;
@@ -15,6 +15,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.assertj.core.util.Lists;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -23,13 +25,12 @@ import uk.gov.hmcts.reform.sscs.bulkscancore.ccd.CaseDataHelper;
 import uk.gov.hmcts.reform.sscs.bulkscancore.domain.CaseResponse;
 import uk.gov.hmcts.reform.sscs.bulkscancore.domain.HandlerResponse;
 import uk.gov.hmcts.reform.sscs.bulkscancore.domain.Token;
-import uk.gov.hmcts.reform.sscs.ccd.domain.Address;
-import uk.gov.hmcts.reform.sscs.ccd.domain.Appeal;
-import uk.gov.hmcts.reform.sscs.ccd.domain.Appellant;
-import uk.gov.hmcts.reform.sscs.ccd.domain.MrnDetails;
+import uk.gov.hmcts.reform.sscs.ccd.domain.*;
+import uk.gov.hmcts.reform.sscs.ccd.service.CcdService;
 import uk.gov.hmcts.reform.sscs.domain.CaseEvent;
 import uk.gov.hmcts.reform.sscs.exceptions.CaseDataHelperException;
 import uk.gov.hmcts.reform.sscs.helper.SscsDataHelper;
+import uk.gov.hmcts.reform.sscs.idam.IdamService;
 
 public class SscsCaseDataHandlerTest {
 
@@ -41,6 +42,12 @@ public class SscsCaseDataHandlerTest {
     @Mock
     CaseDataHelper caseDataHelper;
 
+    @Mock
+    private CcdService ccdService;
+
+    @Mock
+    private IdamService idamService;
+
     LocalDate localDate;
 
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
@@ -49,7 +56,7 @@ public class SscsCaseDataHandlerTest {
     public void setup() {
         initMocks(this);
 
-        sscsCaseDataHandler = new SscsCaseDataHandler(sscsDataHelper, caseDataHelper, new CaseEvent("appealCreated", "validAppealCreated", "incompleteApplicationReceived", "nonCompliant"));
+        sscsCaseDataHandler = new SscsCaseDataHandler(sscsDataHelper, caseDataHelper, new CaseEvent("appealCreated", "validAppealCreated", "incompleteApplicationReceived", "nonCompliant"), ccdService, idamService);
 
         localDate = LocalDate.now();
     }
@@ -64,6 +71,8 @@ public class SscsCaseDataHandlerTest {
         transformedCase.put("appeal", appeal);
 
         CaseResponse caseValidationResponse = CaseResponse.builder().warnings(warnings).transformedCase(transformedCase).build();
+
+        given(ccdService.findCaseBy(any(), any())).willReturn(Lists.emptyList());
 
         given(caseDataHelper.createCase(
             transformedCase, TEST_USER_AUTH_TOKEN, TEST_SERVICE_AUTH_TOKEN, TEST_USER_ID, "incompleteApplicationReceived")).willReturn(1L);
@@ -91,6 +100,8 @@ public class SscsCaseDataHandlerTest {
 
         CaseResponse caseValidationResponse = CaseResponse.builder().warnings(warnings).transformedCase(transformedCase).build();
 
+        given(ccdService.findCaseBy(any(), any())).willReturn(Lists.emptyList());
+
         given(caseDataHelper.createCase(
             transformedCase, TEST_USER_AUTH_TOKEN, TEST_SERVICE_AUTH_TOKEN, TEST_USER_ID, "incompleteApplicationReceived")).willReturn(1L);
 
@@ -100,6 +111,50 @@ public class SscsCaseDataHandlerTest {
         verifyZeroInteractions(caseDataHelper);
 
         assertNull(response);
+    }
+
+    @Test
+    public void givenACaseWithCaseReference_thenDoNotCreateCaseWithIncompleteApplicationEvent() {
+        List<String> warnings = new ArrayList<>();
+
+        Appeal appeal = Appeal.builder().mrnDetails(MrnDetails.builder().mrnDate(localDate.format(formatter)).build()).build();
+        Map<String, Object> transformedCase = new HashMap<>();
+        transformedCase.put("appeal", appeal);
+        transformedCase.put("caseReference",1L);
+
+        CaseResponse caseValidationResponse = CaseResponse.builder().warnings(warnings).transformedCase(transformedCase).build();
+
+        CallbackResponse response =  sscsCaseDataHandler.handle(caseValidationResponse, false,
+                Token.builder().userAuthToken(TEST_USER_AUTH_TOKEN).serviceAuthToken(TEST_SERVICE_AUTH_TOKEN).userId(TEST_USER_ID).build(), null);
+
+        verifyZeroInteractions(caseDataHelper);
+    }
+
+    @Test
+    public void givenACaseWithExistingNinoMrnDateAndBenefitCode_thenDoNotCreateCaseWithIncompleteApplicationEvent() {
+        SscsCaseDetails sscsCaseDetails = mock(SscsCaseDetails.class);
+        List<SscsCaseDetails> caseDetails = new ArrayList<>();
+        caseDetails.add(sscsCaseDetails);
+        String nino = "testnino";
+        String benifitCode = "002";
+        LocalDate mrnDate = LocalDate.of(2019,8, 2);
+
+        Appeal appeal = Appeal.builder().mrnDetails(MrnDetails.builder().mrnDate(mrnDate.format(formatter)).build())
+                .appellant(Appellant.builder().identity(Identity.builder().nino(nino).build()).build())
+                .benefitType(BenefitType.builder().code(benifitCode).build()).build();
+        Map<String, Object> transformedCase = new HashMap<>();
+        transformedCase.put("appeal", appeal);
+        transformedCase.put("generatedNino",nino);
+        transformedCase.put("benefitCode", benifitCode);
+
+        given(ccdService.findCaseBy(anyMap(), any())).willReturn(caseDetails);
+
+        CaseResponse caseValidationResponse = CaseResponse.builder().warnings(new ArrayList<>()).transformedCase(transformedCase).build();
+
+        CallbackResponse response =  sscsCaseDataHandler.handle(caseValidationResponse, false,
+                Token.builder().userAuthToken(TEST_USER_AUTH_TOKEN).serviceAuthToken(TEST_SERVICE_AUTH_TOKEN).userId(TEST_USER_ID).build(), null);
+
+        verifyZeroInteractions(caseDataHelper);
     }
 
     @Test
@@ -114,6 +169,8 @@ public class SscsCaseDataHandlerTest {
         transformedCase.put("appeal", appeal);
 
         CaseResponse caseValidationResponse = CaseResponse.builder().transformedCase(transformedCase).build();
+
+        given(ccdService.findCaseBy(any(), any())).willReturn(Lists.emptyList());
 
         given(caseDataHelper.createCase(
             transformedCase, TEST_USER_AUTH_TOKEN, TEST_SERVICE_AUTH_TOKEN, TEST_USER_ID, "validAppealCreated")).willReturn(1L);
@@ -140,6 +197,8 @@ public class SscsCaseDataHandlerTest {
         transformedCase.put("appeal", appeal);
 
         CaseResponse caseValidationResponse = CaseResponse.builder().transformedCase(transformedCase).build();
+
+        given(ccdService.findCaseBy(any(), any())).willReturn(Lists.emptyList());
 
         given(caseDataHelper.createCase(
             transformedCase, TEST_USER_AUTH_TOKEN, TEST_SERVICE_AUTH_TOKEN, TEST_USER_ID, "nonCompliant")).willReturn(1L);
@@ -169,6 +228,8 @@ public class SscsCaseDataHandlerTest {
         transformedCase.put("appeal", appeal);
 
         CaseResponse caseValidationResponse = CaseResponse.builder().warnings(warnings).transformedCase(transformedCase).build();
+
+        given(ccdService.findCaseBy(any(), any())).willReturn(Lists.emptyList());
 
         given(caseDataHelper.createCase(
             transformedCase, TEST_USER_AUTH_TOKEN, TEST_SERVICE_AUTH_TOKEN, TEST_USER_ID, "incompleteApplicationReceived")).willReturn(1L);
