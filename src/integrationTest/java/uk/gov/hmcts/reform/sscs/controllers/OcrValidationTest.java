0f19dfb69decd0784064ee00dc2e9f83682a2945
jackmaloney <jack.maloney@transformuk.com>
2020-04-16 15:58:40 +0100

    Remove wiremock from OcrValidationTest

--- src/integrationTest/java/uk/gov/hmcts/reform/sscs/controllers/OcrValidationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sscs/controllers/OcrValidationTest.java
@@ -9,21 +9,35 @@ import static uk.gov.hmcts.reform.sscs.helper.TestConstants.SERVICE_AUTH_TOKEN;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import java.io.IOException;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.sscs.BaseTest;
+import uk.gov.hmcts.reform.sscs.bulkscancore.domain.ExceptionCaseData;
 
 
 @AutoConfigureMockMvc
 @ContextConfiguration
-class OcrValidationTest extends BaseTest  {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application_it.yaml")
+class OcrValidationTest  {
 
     @Autowired
     private MockMvc mvc;
+
+    @MockBean
+    protected AuthTokenValidator authTokenValidator;
 
     @Test
     void should_return_200_when_ocr_form_validation_request_data_is_valid() throws Throwable {
