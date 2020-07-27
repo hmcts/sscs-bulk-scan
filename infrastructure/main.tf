59da0d10aa23bc8e82e39d218edb37814876a653
sunil <sunil.rochani@hmcts.net>
2020-07-27 11:56:25 +0100

    Updated new MI for all envs

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,6 +8,11 @@ resource "azurerm_resource_group" "rg" {
   location = "${var.location_app}"
 }
 
+data "azurerm_user_assigned_identity" "sscs-identity" {
+  name                = "sscs-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 locals {
   vaultName = "sscs-bulk-scan-${var.env}"
 }
@@ -22,6 +27,5 @@ module "sscs-bulk-scan-vault" {
   resource_group_name     = "${azurerm_resource_group.rg.name}"
   product_group_object_id = "70de400b-4f47-4f25-a4f0-45e1ee4e4ae3"
   common_tags             = "${var.common_tags}"
-
-  managed_identity_object_id = "${var.managed_identity_object_id}"
+  managed_identity_object_ids = ["${data.azurerm_user_assigned_identity.sscs-identity.principal_id}","${var.managed_identity_object_id}"]
 }
--- infrastructure/prod.tfvars
+++ infrastructure/prod.tfvars
@@ -1,1 +1,1 @@
-managed_identity_object_id="964aecb6-9993-47fb-81b5-ed3e17cc48ec"
+
