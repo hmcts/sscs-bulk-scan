70a5a88feb1142782abf56f7a91125a548850b4b
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2020-07-27 12:11:56 +0100

    Update infrastructure/main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -27,5 +27,5 @@ module "sscs-bulk-scan-vault" {
   resource_group_name     = "${azurerm_resource_group.rg.name}"
   product_group_object_id = "70de400b-4f47-4f25-a4f0-45e1ee4e4ae3"
   common_tags             = "${var.common_tags}"
-  managed_identity_object_ids = ["${data.azurerm_user_assigned_identity.sscs-identity.principal_id}","${var.managed_identity_object_id}"]
+  managed_identity_object_ids = ["${data.azurerm_user_assigned_identity.sscs-identity.principal_id}"]
 }
