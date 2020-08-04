43394d8848ce25dd835a07d9a3d7d8d2fcfd3692
sunil <sunil.rochani@hmcts.net>
2020-08-04 16:29:52 +0100

    perftest bulk-scan update

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,6 +15,8 @@ data "azurerm_user_assigned_identity" "sscs-identity" {
 
 locals {
   vaultName = "sscs-bulk-scan-${var.env}"
+  preview_vault_name = "https://${var.raw_product}-aat.vault.azure.net/"
+  permanent_vault_uri = "${var.env != "preview" ? module.sscs-bulk-scan-vault.key_vault_uri : local.preview_vault_name}"
 }
 
 module "sscs-bulk-scan-vault" {
@@ -28,4 +30,30 @@ module "sscs-bulk-scan-vault" {
   product_group_object_id = "70de400b-4f47-4f25-a4f0-45e1ee4e4ae3"
   common_tags             = "${var.common_tags}"
   managed_identity_object_ids = ["${data.azurerm_user_assigned_identity.sscs-identity.principal_id}"]
+}
+
+data "azurerm_key_vault_secret" "sscs_s2s_secret" {
+  name      = "sscs-s2s-secret"
+  vault_uri = "${local.permanent_vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "idam_sscs_systemupdate_user" {
+  name      = "idam-sscs-systemupdate-user"
+  vault_uri = "${local.permanent_vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "idam_sscs_systemupdate_password" {
+  name      = "idam-sscs-systemupdate-password"
+  vault_uri = "${local.permanent_vault_uri}"
+}
+
+data "azurerm_key_vault_secret" "idam_sscs_oauth2_client_secret" {
+  name      = "idam-sscs-oauth2-client-secret"
+  vault_uri = "${local.permanent_vault_uri}"
+}
+
+
+data "azurerm_key_vault_secret" "appinsights_instrumentation_key" {
+  name      = "AppInsightsInstrumentationKey"
+  vault_uri = "${local.permanent_vault_uri}"
 }
