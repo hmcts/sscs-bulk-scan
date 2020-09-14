504e35bb2fcd39eb5181ba8cf9e67bdeb019baef
Ryan Da Costa <369407+ryandac@users.noreply.github.com>
2020-09-14 10:08:35 +0100

    Upgrade terraform (#671)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,11 @@
 provider "azurerm" {
-  version = "1.44.0"
+  features {}
 }
 
 # Make sure the resource group exists
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
-  location = "${var.location_app}"
+  location = var.location_app
 }
 
 data "azurerm_user_assigned_identity" "sscs-identity" {
@@ -19,13 +19,13 @@ locals {
 
 module "sscs-bulk-scan-vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                    = "${local.vaultName}"
-  product                 = "${var.product}"
-  env                     = "${var.env}"
-  tenant_id               = "${var.tenant_id}"
-  object_id               = "${var.jenkins_AAD_objectId}"
-  resource_group_name     = "${azurerm_resource_group.rg.name}"
+  name                    = local.vaultName
+  product                 = var.product
+  env                     = var.env
+  tenant_id               = var.tenant_id
+  object_id               = var.jenkins_AAD_objectId
+  resource_group_name     = azurerm_resource_group.rg.name
   product_group_object_id = "70de400b-4f47-4f25-a4f0-45e1ee4e4ae3"
-  common_tags             = "${var.common_tags}"
+  common_tags             = var.common_tags
   managed_identity_object_ids = ["${data.azurerm_user_assigned_identity.sscs-identity.principal_id}"]
 }
