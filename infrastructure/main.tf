26d6552e8cec15d3f502995ab8e8f6dcb404acf9
sunilrochani <sunil.rochani@hotmail.com>
2020-09-23 11:32:28 +0100

    SSCS-8161: Tech debt – Fix bulk scan AppInsights

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_application_insights" "appinsights" {
 resource "azurerm_key_vault_secret" "app_insights_key" {
   name         = "AppInsightsInstrumentationKey"
   value        = azurerm_application_insights.appinsights.instrumentation_key
-  key_vault_id = module.sscs-bulk-scan-vault.key_vault_id
+  key_vault_id = module.sscs-vault.key_vault_id
 }
 
 output "appInsightsInstrumentationKey" {
