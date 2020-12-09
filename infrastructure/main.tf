a62669cf153a83565c4e3bd1e882ba5ced0cfcbe
sunilrochani <sunil.rochani@hotmail.com>
2020-12-09 00:28:08 +0000

    SSCS-8285: Monitoring – Combine AppInsights sscs-bulk-scan into sscs-prod

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -43,9 +43,16 @@ data "azurerm_key_vault" "sscs_key_vault" {
   resource_group_name = local.sscsRg
 }
 
+data "azurerm_application_insights" "sscsappinsights" {
+  name                = "sscs-${var.env}"
+  location            = var.location_app
+  resource_group_name = local.sscsRg
+  application_type    = var.appinsights_application_type
+}
+
 resource "azurerm_key_vault_secret" "app_insights_key" {
   name         = "AppInsightsInstrumentationKey"
-  value        = appInsightsInstrumentationKey
+  value        = azurerm_application_insights.appinsights.instrumentation_key
   key_vault_id = module.sscs-bulk-scan-vault.key_vault_id
 }
 
