76e19049b5a0add4f433a4775373529634c08cf3
srochani <sunil.rochani@hotmail.com>
2021-01-25 16:37:55 +0000

    Supress unwanted logs (#802)
    * SSCS-8161: Tech debt – Fix bulk scan AppInsights
    
    * SSCS-8161: Tech debt – Fix bulk scan AppInsights
    
    * SSCS-8161: Tech debt – Fix bulk scan AppInsights
    
    * SSCS-8161: Tech debt – Fix bulk scan AppInsights
    
    * SSCS-8161: Tech debt – Fix bulk scan AppInsights
    
    * SSCS-8285: Monitoring – Combine AppInsights sscs-bulk-scan into sscs-prod
    
    * SSCS-8285: Monitoring – Combine AppInsights sscs-bulk-scan into sscs-prod
    
    * SSCS-8285: Monitoring – Combine AppInsights sscs-bulk-scan into sscs-prod
    
    * SSCS-8285: Monitoring – Combine AppInsights sscs-bulk-scan into sscs-prod
    
    * SSCS-8285: Monitoring – Combine AppInsights sscs-bulk-scan into sscs-prod
    
    * SSCS-8285: Monitoring – Combine AppInsights sscs-bulk-scan into sscs-prod
    
    * SSCS-8285: Monitoring – Combine AppInsights sscs-bulk-scan into sscs-prod
    
    * SSCS-8285: Monitoring – Combine AppInsights sscs-bulk-scan into sscs-prod
    
    * Suppress unwanted logs
    
    * Update main.tf
    
    * Update main.tf

--- src/main/java/uk/gov/hmcts/reform/sscs/validators/SscsCaseValidator.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/validators/SscsCaseValidator.java
@@ -506,7 +506,6 @@ public class SscsCaseValidator implements CaseValidator {
 
     private void checkHearingSubTypeIfHearingIsOral(Appeal appeal, Map<String, Object> caseData) {
         String hearingType = appeal.getHearingType();
-        log.info("Bulk-scan entry set --------------" + caseData.entrySet().toString());
         FormType formType = (FormType) caseData.get("formType");
         log.info("Bulk-scan form type: {}", formType != null ? formType.toString() : null);
         if (FormType.SSCS1PEU.equals(formType) && hearingType != null && hearingType.equals(HEARING_TYPE_ORAL) && !isValidHearingSubType(appeal)) {
