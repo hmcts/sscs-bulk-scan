bcadcd1a377cb1f45e50a31a24847a3e36081c28
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-07-15 16:44:41 +0100

    SSCS-9405 - Fuzzy matching of OCR'd Benefit names

--- src/main/java/uk/gov/hmcts/reform/sscs/service/FuzzyMatcherService.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/service/FuzzyMatcherService.java
@@ -1,51 +1,97 @@
 package uk.gov.hmcts.reform.sscs.service;
 
+import static java.util.Arrays.stream;
+import static java.util.Optional.ofNullable;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.Benefit.*;
+import static uk.gov.hmcts.reform.sscs.ccd.domain.Benefit.findBenefitByDescription;
+
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
+import me.xdrop.fuzzywuzzy.FuzzySearch;
+import me.xdrop.fuzzywuzzy.model.ExtractedResult;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.sscs.ccd.domain.Benefit;
-import uk.gov.hmcts.reform.sscs.domain.ExactMatchList;
-import uk.gov.hmcts.reform.sscs.domain.FuzzyMatchList;
 
 @Service
 @Slf4j
 public class FuzzyMatcherService {
+    private static final int THRESHOLD_SCORE = 90;
 
-    private static List<String> pipFuzzyList = new FuzzyMatchList();
-    private static List<String> pipExactMatchList = new ExactMatchList();
-    private static List<String> esaFuzzyList = new FuzzyMatchList();
-    private static List<String> esaExactMatchList = new ExactMatchList();
-    private static List<String> ucFuzzyList = new FuzzyMatchList();
-    private static List<String> ucExactMatchList = new ExactMatchList();
+    private static final Map<String, Benefit> FUZZY_WORD_MATCH_MAP = Map.of(
+        "personal", PIP,
+        "independence", PIP,
+        "universal", UC,
+        "employment", ESA
+    );
+
+    private static final Map<String, Benefit> EXACT_WORD_MATCH = Map.ofEntries(
+        Map.entry("AA", ATTENDANCE_ALLOWANCE),
+        Map.entry("IS", INCOME_SUPPORT),
+        Map.entry("liv", DLA),
+        Map.entry("SF", SOCIAL_FUND),
+        Map.entry("PC", PENSION_CREDITS),
+        Map.entry("RP", RETIREMENT_PENSION),
+        Map.entry("BB", BEREAVEMENT_BENEFIT),
+        Map.entry("MA", MATERNITY_ALLOWANCE),
+        Map.entry("IIDB", IIDB),
+        Map.entry("BSPS", BEREAVEMENT_SUPPORT_PAYMENT_SCHEME),
+        Map.entry("BSPA", BEREAVEMENT_SUPPORT_PAYMENT_SCHEME)
+    );
+
+    private final List<String> fuzzyChoices;
 
     public FuzzyMatcherService() {
-        pipFuzzyList.add("personal");
-        pipFuzzyList.add("independence");
-        pipFuzzyList.add("p.i.p");
-
-        pipExactMatchList.add("pip");
-
-        esaFuzzyList.add("employment");
-        esaFuzzyList.add("support");
-        esaFuzzyList.add("e.s.a");
-
-        esaExactMatchList.add("esa");
-
-        ucFuzzyList.add("universal");
-        ucFuzzyList.add("credit");
-        ucFuzzyList.add("u.c");
-
-        ucExactMatchList.add("uc");
+        List<String> allMatches = stream(Benefit.values()).flatMap(benefit -> Stream.of(benefit.getShortName(), benefit.getDescription())).collect(Collectors.toList());
+        allMatches.addAll(FUZZY_WORD_MATCH_MAP.keySet());
+        fuzzyChoices = Collections.unmodifiableList(allMatches);
     }
 
     public String matchBenefitType(String ocrBenefitValue) {
-        if (pipFuzzyList.contains(ocrBenefitValue) || pipExactMatchList.contains(ocrBenefitValue)) {
-            return Benefit.PIP.name();
-        } else if (esaFuzzyList.contains(ocrBenefitValue) || esaExactMatchList.contains(ocrBenefitValue)) {
-            return Benefit.ESA.name();
-        } else if (ucFuzzyList.contains(ocrBenefitValue) || ucExactMatchList.contains(ocrBenefitValue)) {
-            return Benefit.UC.name();
-        }
-        return ocrBenefitValue;
+        return getBenefitByCode(ocrBenefitValue).map(Benefit::getShortName).orElse(ocrBenefitValue);
     }
+
+    private Optional<Benefit> getBenefitByCode(String code) {
+        final ExtractedResult extractedResult = FuzzySearch.extractOne(code.replaceAll("\\.",""), fuzzyChoices);
+        logMessage(code, extractedResult);
+        String fuzzyCode = getSearchCodeBasedOnScoreThreshold(code, extractedResult);
+        return getBenefit(fuzzyCode);
+    }
+
+    private String getSearchCodeBasedOnScoreThreshold(String code, ExtractedResult extractedResult) {
+        return (extractedResult.getScore() < THRESHOLD_SCORE) ? code : extractedResult.getString();
+    }
+
+    private void logMessage(String code, ExtractedResult extractedResult) {
+        log.info("Search code {} has a fuzzy match score of {} with {}. The threshold score is {}. {} the fuzzy match.",
+            code, extractedResult.getScore(), extractedResult.getString(), THRESHOLD_SCORE,
+            extractedResult.getScore() < THRESHOLD_SCORE ? "Not Using" : "Using");
+    }
+
+    private Optional<Benefit> getBenefit(String fuzzyCode) {
+        return ofNullable(findBenefitByShortName(fuzzyCode)
+            .orElseGet(() -> findBenefitByDescription(fuzzyCode)
+                    .orElseGet(() -> findBenefitByFuzzyExactWord(fuzzyCode)
+                    .orElseGet(() -> findBenefitByExactWord(fuzzyCode)
+                        .orElse(null)))));
+    }
+
+    private static Optional<Benefit> findBenefitByFuzzyExactWord(String code) {
+        return FUZZY_WORD_MATCH_MAP.keySet().stream()
+            .filter(key -> key.equalsIgnoreCase(code))
+            .findFirst()
+            .map(FUZZY_WORD_MATCH_MAP::get);
+    }
+
+    private static Optional<Benefit> findBenefitByExactWord(String code) {
+        return EXACT_WORD_MATCH.keySet().stream()
+            .filter(key -> key.equalsIgnoreCase(code.replaceAll("\\.", "")))
+            .findFirst()
+            .map(EXACT_WORD_MATCH::get);
+    }
+
 }
--- src/test/java/uk/gov/hmcts/reform/sscs/service/FuzzyMatcherServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/service/FuzzyMatcherServiceTest.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.sscs.service;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -8,6 +10,7 @@ import junitparams.Parameters;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import uk.gov.hmcts.reform.sscs.ccd.domain.Benefit;
 
 @RunWith(JUnitParamsRunner.class)
 public class FuzzyMatcherServiceTest {
@@ -43,6 +46,100 @@ public class FuzzyMatcherServiceTest {
         String result = fuzzyMatcherService.matchBenefitType(ocrValue);
 
         assertEquals("UC", result);
+    }
+
+    @Test
+    @Parameters({"unknown", "sdfhkjsdh", "fisls", "livewitre", "dlsb", "iida", "personel"})
+    public void unknownCodeWillBeReturned(String unknownCode) {
+        assertThat(fuzzyMatcherService.matchBenefitType(unknownCode), is(unknownCode));
+    }
+
+    @Test
+    @Parameters({
+        "pip, PIP",
+        "esa, ESA",
+        "Attendance, ATTENDANCE_ALLOWANCE",
+        "att, ATTENDANCE_ALLOWANCE",
+        "A.A, ATTENDANCE_ALLOWANCE",
+        "AA, ATTENDANCE_ALLOWANCE",
+        "Disability, DLA",
+        "Living, DLA",
+        "liv, DLA",
+        "livi, DLA",
+        "livin, DLA",
+        "D.L.A, DLA",
+        "D.L.A, DLA",
+        "DLA, DLA",
+        "DLA test, DLA",
+        "Disability Living Allowance, DLA",
+        "Income, INCOME_SUPPORT",
+        "inc, INCOME_SUPPORT",
+        "inco, INCOME_SUPPORT",
+        "incom, INCOME_SUPPORT",
+        "Income Support, INCOME_SUPPORT",
+        "incomesupport, INCOME_SUPPORT",
+        "incomesupport test, INCOME_SUPPORT",
+        "I.S, INCOME_SUPPORT",
+        "Injuries, IIDB",
+        "Disablement, IIDB",
+        "IIDB, IIDB",
+        "I.I.D.B, IIDB",
+        "Industrial Injuries Disablement Benefit, IIDB",
+        "Job, JSA",
+        "JSA, JSA",
+        "J.S.A, JSA",
+        "Job Seekers Allowance, JSA",
+        "jobseekersallowance, JSA",
+        "Social, SOCIAL_FUND",
+        "Fund, SOCIAL_FUND",
+        "SF, SOCIAL_FUND",
+        "Social Fund', SOCIAL_FUND",
+        "socialfund', SOCIAL_FUND",
+        "PC, PENSION_CREDITS",
+        "P.C, PENSION_CREDITS",
+        "Pension Credits', PENSION_CREDITS",
+        "pensioncredits', PENSION_CREDITS",
+        "Retirement, RETIREMENT_PENSION",
+        "Ret, RETIREMENT_PENSION",
+        "Reti, RETIREMENT_PENSION",
+        "Retir, RETIREMENT_PENSION",
+        "Retire, RETIREMENT_PENSION",
+        "Retirem, RETIREMENT_PENSION",
+        "Retireme, RETIREMENT_PENSION",
+        "Retiremen, RETIREMENT_PENSION",
+        "RP, RETIREMENT_PENSION",
+        "R.P, RETIREMENT_PENSION",
+        "Retirement Pension, RETIREMENT_PENSION",
+        "retirementpension, RETIREMENT_PENSION",
+        "BB, BEREAVEMENT_BENEFIT",
+        "B.B, BEREAVEMENT_BENEFIT",
+        "Bereavement Benefit, BEREAVEMENT_BENEFIT",
+        "bereavementbenefit, BEREAVEMENT_BENEFIT",
+        "Carers, CARERS_ALLOWANCE",
+        "Carers, CARERS_ALLOWANCE",
+        "Care, CARERS_ALLOWANCE",
+        "Car, CARERS_ALLOWANCE",
+        "C.A, CARERS_ALLOWANCE",
+        "Carer's Allowance, CARERS_ALLOWANCE",
+        "carersallowance, CARERS_ALLOWANCE",
+        "Maternity, MATERNITY_ALLOWANCE",
+        "Mat, MATERNITY_ALLOWANCE",
+        "Mate, MATERNITY_ALLOWANCE",
+        "Mater, MATERNITY_ALLOWANCE",
+        "Matern, MATERNITY_ALLOWANCE",
+        "Materni, MATERNITY_ALLOWANCE",
+        "Maternit, MATERNITY_ALLOWANCE",
+        "M.A, MATERNITY_ALLOWANCE",
+        "Maternity Allowance', MATERNITY_ALLOWANCE",
+        "maternityallowance', MATERNITY_ALLOWANCE",
+        "BSPS, BEREAVEMENT_SUPPORT_PAYMENT_SCHEME",
+        "B.S.P.A, BEREAVEMENT_SUPPORT_PAYMENT_SCHEME",
+        "Bereavement Support Payment Scheme', BEREAVEMENT_SUPPORT_PAYMENT_SCHEME",
+        "bereavementsupportpaymentscheme', BEREAVEMENT_SUPPORT_PAYMENT_SCHEME",
+    })
+    public void getBenefitByCode(String code, Benefit expectedBenefit) {
+        final String result = fuzzyMatcherService.matchBenefitType(code);
+        assertThat(result, is(expectedBenefit.getShortName()));
     }
 
 }
