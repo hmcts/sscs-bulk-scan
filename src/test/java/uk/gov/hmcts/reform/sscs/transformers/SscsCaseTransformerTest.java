834b02afd9aad84a1c08054c172cf5e31203d1a3
Chris Davidson <christopher.davidson@btinternet.com>
2021-07-21 12:14:16 +0100

    sscs 9403 updated test names

--- src/test/java/uk/gov/hmcts/reform/sscs/transformers/SscsCaseTransformerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/transformers/SscsCaseTransformerTest.java
@@ -290,7 +290,7 @@ public class SscsCaseTransformerTest {
     }
 
     @Test
-    public void givenBenefitTypePipWithIsOtherBenefitYes_thenErrorMessage() {
+    public void givenBenefitTypeEsaAndUcWithIsOtherBenefitYes_thenErrorMessage() {
         pairs.remove("is_benefit_type_pip");
         pairs.put(BenefitTypeIndicatorSscs1U.PIP.getIndicatorString(), "No");
         pairs.put(IS_BENEFIT_TYPE_OTHER, "Yes");
@@ -302,7 +302,7 @@ public class SscsCaseTransformerTest {
     }
 
     @Test
-    public void givenBenefitTypePipWithIsOtherBenefitTrue_thenErrorMessage() {
+    public void givenBenefitTypeEsaAndUcWithIsOtherBenefitTrue_thenErrorMessage() {
         pairs.remove("is_benefit_type_pip");
         pairs.put(BenefitTypeIndicatorSscs1U.PIP.getIndicatorString(), false);
         pairs.put(IS_BENEFIT_TYPE_OTHER, true);
