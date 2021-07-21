302b7de687dfc951297c6db985f5e7b1ed98cf17
Chris Davidson <christopher.davidson@btinternet.com>
2021-07-21 12:00:43 +0100

    sscs 9403 update to be consistent

--- src/test/java/uk/gov/hmcts/reform/sscs/constants/BenefitTypeIndicatorSscs1UTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/constants/BenefitTypeIndicatorSscs1UTest.java
@@ -14,16 +14,16 @@ public class BenefitTypeIndicatorSscs1UTest {
 
     @Test
     public void testAllBenefitTypesIndicatorsAreConfiguredCorrectly() {
-        for (BenefitTypeIndicatorSscs1U benefitTypeIndicator : BenefitTypeIndicatorSscs1U.values()) {
-            // Assert that each BenefitTypeIndicator enum has both an indicator string and a benefit
-            assertNotNull(benefitTypeIndicator.getIndicatorString());
-            if (!benefitTypeIndicator.getIndicatorString().equals("is_benefit_type_other")) {
-                assertNotNull(benefitTypeIndicator.getBenefit());
+        for (BenefitTypeIndicatorSscs1U benefitTypeIndicatorSscs1U : BenefitTypeIndicatorSscs1U.values()) {
+            // Assert that each BenefitTypeIndicatorSscs1U enum has both an indicator string and a benefit except other
+            assertNotNull(benefitTypeIndicatorSscs1U.getIndicatorString());
+            if (!benefitTypeIndicatorSscs1U.getIndicatorString().equals("is_benefit_type_other")) {
+                assertNotNull(benefitTypeIndicatorSscs1U.getBenefit());
                 // Assert that if we lookup the benefit type indicator via it's indicator string, we
                 // find the same benefit as has been configured.
-                Optional<Benefit> benefitTypeLookup = BenefitTypeIndicator.findByIndicatorString(benefitTypeIndicator.getIndicatorString());
+                Optional<Benefit> benefitTypeLookup = BenefitTypeIndicatorSscs1U.findByIndicatorString(benefitTypeIndicatorSscs1U.getIndicatorString());
                 assertTrue(benefitTypeLookup.isPresent());
-                assertEquals(benefitTypeLookup.get(), benefitTypeIndicator.getBenefit());
+                assertEquals(benefitTypeLookup.get(), benefitTypeIndicatorSscs1U.getBenefit());
             }
         }
     }
