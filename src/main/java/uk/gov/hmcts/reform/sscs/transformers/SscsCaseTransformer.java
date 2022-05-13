4ae5d69ac0d0dac6bd343fa44b1d2791151c016c
Waqas Arshid <waqas.arshid@hmcts.net>
2022-05-13 15:52:39 +0100

    SSCS-10490 removed unused import

--- src/main/java/uk/gov/hmcts/reform/sscs/transformers/SscsCaseTransformer.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/transformers/SscsCaseTransformer.java
@@ -28,7 +28,6 @@ import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.sscs.bulkscancore.domain.*;
