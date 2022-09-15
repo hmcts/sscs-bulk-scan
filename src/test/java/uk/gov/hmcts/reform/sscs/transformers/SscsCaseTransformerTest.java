13af2dd7ce1cd9046fa63c30318ed234f67dab2a
Benson Li <benson.li@evoco.co.uk>
2022-09-15 15:08:58 +0100

    code style update

--- src/test/java/uk/gov/hmcts/reform/sscs/transformers/SscsCaseTransformerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/transformers/SscsCaseTransformerTest.java
@@ -2560,7 +2560,7 @@ public class SscsCaseTransformerTest {
     }
 
     @SuppressWarnings("unchecked")
-    private void assertSscsDocumentFormType(CaseResponse result, String formType){
+    private void assertSscsDocumentFormType(CaseResponse result, String formType) {
         Object resultObject = result.getTransformedCase().get("sscsDocument");
         Assert.isInstanceOf(List.class, resultObject);
 
