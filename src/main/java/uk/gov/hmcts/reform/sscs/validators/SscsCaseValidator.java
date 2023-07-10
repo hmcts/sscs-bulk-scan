18b3c3b1e0c72cd1a686bdae8fe148d90009ef2f
hajam09 <hajam09@yahoo.com>
2023-07-10 10:43:12 +0100

    Update

--- src/main/java/uk/gov/hmcts/reform/sscs/validators/SscsCaseValidator.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/validators/SscsCaseValidator.java
@@ -758,6 +758,9 @@ public class SscsCaseValidator implements CaseValidator {
                 checkExcludeStartDateAndEndDate(excludeDate);
             }
         }
+        if (appeal.getHearingOptions().getExcludeDates() == null) {
+            errors.add("Add an start date for unavailable dates");
+        }
     }
 
     private void checkMobileNumber(Contact contact, String personType) {
