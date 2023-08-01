0df0e64d2fb18e016f1ca399ac0d206c25c9b82e
Andrei Trushkov <andrei.trushkov@hmcts.net>
2023-08-01 12:06:33 +0100

    appinsight connection string

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -55,6 +55,12 @@ output "appInsightsInstrumentationKey" {
   value = data.azurerm_application_insights.sscsappinsights.instrumentation_key
 }
 
+resource "azurerm_key_vault_secret" "app_insights_connection_string" {
+  name         = "app-insights-connection-string"
+  value        = azurerm_application_insights.appinsights.connection_string
+  key_vault_id = module.sscs-vault.key_vault_id
+}
+
 
 
 
