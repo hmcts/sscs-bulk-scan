9cecf37cc324c73a32532c2d4a868ba1b8f49ab4
Katalin Kovacs <katalin.kovacs@solirius.com>
2024-09-18 11:10:21 +0100

    Added EnableWebMvc annotation

--- src/main/java/uk/gov/hmcts/reform/sscs/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/SwaggerConfiguration.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.sscs.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -9,6 +10,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import uk.gov.hmcts.reform.sscs.BulkScanApplication;
 
 @Configuration
+@EnableWebMvc
 public class SwaggerConfiguration {
 
     @Bean
