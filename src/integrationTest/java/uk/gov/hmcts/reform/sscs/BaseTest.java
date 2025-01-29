ab84bedde9430f350cb7fb4d60227d74b9c6e8b7
Linus Norton <linusnorton@gmail.com>
2025-01-29 12:05:08 +0000

    spring boot 3 migration

--- src/integrationTest/java/uk/gov/hmcts/reform/sscs/BaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sscs/BaseTest.java
@@ -19,7 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
